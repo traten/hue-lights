b0fd85c3ada3941b195320681e563d905f32306c
Trevor Aten <traten11@gmail.com>
2024-03-24 12:22:25 -0400

    move desk left

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -3,15 +3,13 @@ const constants = require('../constants/constants.json');
 const HueBridge = require('../constants/HueBridge.json');
 const fetch = require('node-fetch');
 
-const DeskLeft = '6019c7bc-e447-4c66-ba06-76be3d494535';
-
 const hue = HueApi(fetch, HueBridge.IPAddress, HueBridge.UserId);
 
 const run = async () => {
   const deviceList = await hue.getDeviceList();
   console.log(deviceList);
 
-  const success = await hue.turnLightState(DeskLeft, constants.ON);
+  const success = await hue.turnLightState(HueBridge.DeskLeft, constants.ON);
   console.log(success);
 };
 
