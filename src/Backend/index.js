b4eddd255d96b7dcbcc85d3ccaf008934eab720c
Trevor Aten <traten11@gmail.com>
2024-03-24 13:37:01 -0400

    CI Setup (#2)
    * let's see
    
    * new name
    
    * hmm
    
    * renames
    
    * env file

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -1,15 +1,16 @@
 const HueApi = require('./hueAPI.js');
 const constants = require('../constants/constants.json');
-const HueBridge = require('../constants/HueBridge.json');
 const fetch = require('node-fetch');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const hue = HueApi(fetch, HueBridge.IPAddress, HueBridge.UserId);
+const hue = HueApi(fetch, process.env.IPAddress, process.env.UserId);
 
 const run = async () => {
   const deviceList = await hue.getDeviceList();
   console.log(deviceList);
 
-  const success = await hue.turnLightState(HueBridge.DeskLeft, constants.ON);
+  const success = await hue.turnLightState(process.env.DeskLeft, constants.ON);
   console.log(success);
 };
 
